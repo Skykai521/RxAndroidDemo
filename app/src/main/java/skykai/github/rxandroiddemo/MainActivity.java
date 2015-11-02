f3d3d13175c44366bbc80a39e27a0fc9c400d88e
skykai <daqingkai@boohee.com>
2015-11-02 09:35:26 +0800

    add retrolambda

--- app/src/main/java/skykai/github/rxandroiddemo/MainActivity.java
+++ app/src/main/java/skykai/github/rxandroiddemo/MainActivity.java
@@ -7,6 +7,10 @@ import android.util.Log;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.content_main);
 
 
-        firstSample();
+//        firstSample();
+//        secondSample();
+//        thirdSample();
     }
 
+    //基本用法
     private void firstSample() {
-
         // RxJava 有四个基本概念：Observable (可观察者，即被观察者)、
         // Observer (观察者)、 subscribe (订阅)、事件。Observable 和 Observer 通过 subscribe()
         // 方法实现订阅关系，从而 Observable 可以在需要的时候发出事件来通知 Observer。
@@ -46,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNext(String s) {
-                Log.d(TAG,"myObserver1 : " + s);
+                Log.d(TAG, "myObserver1 : " + s);
             }
         };
         myObservable1.subscribe(myObserver1);
@@ -66,10 +72,87 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNext(String s) {
-                Log.d(TAG,"mySubscriber1 : " + s);
+                Log.d(TAG, "mySubscriber1 : " + s);
             }
         };
 
         myObservable1.subscribe(mySubscriber1);
+
+        Observable.just("Hello World")
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.d(TAG, s);
+                    }
+                });
+    }
+
+    //简化用法 操作符
+    private void secondSample() {
+        //map操作符更有趣的一点是它不必返回Observable对象返回的类型，你可以使用map操作符返回一个发出新的数据类型的observable对象。
+        Observable.just("Hello World")
+                .map(new Func1<String, Integer>() {
+                    @Override
+                    public Integer call(String s) {
+                        return s.hashCode();
+                    }
+                })
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        Log.d(TAG, "Integer : " + integer.toString());
+                    }
+                });
+
+        Observable.just("Hello Wrold2")
+                .map(s -> s.hashCode())
+                .subscribe(s -> Log.d(TAG, "Integer : " + s.toString()));
+    }
+
+    private void thirdSample() {
+        String[] names = {"AAA", "BBBB", "CCCC", "DDD"};
+        Observable.from(names)
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.d(TAG, "names : " + s);
+                    }
+                });
+
+        Observable.from(names)
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return s;
+                    }
+                })
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.d(TAG, "names2 : " + s);
+                    }
+                });
+
+        Observable.from(names)
+                .map(s -> {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return s;
+                })
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(s -> {
+                    Log.d(TAG, "names3 : " + s);
+                });
     }
 }
